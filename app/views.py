19f83afdb1bde64b54ea8bf1bb85c885c58a454a
Abi123-suara <abinash998143@gmail.com>
2023-11-08 14:29:35 +0530

    first commit

---
+++ app/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+
+def abinash(request):
+    return HttpResponse("<h1><marquee>heyy it's Abinash</marquee></h1>")
+
+
+# Create your views here.
